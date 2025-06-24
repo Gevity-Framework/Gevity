170583c606c809ab899f6c879ca78955a9bb882c
phillipstudios <yemmyuwu@gmail.com>
2025-06-24 14:53:57 -0500

    cargo fmt

--- mcp/deploy/update.sh
+++ mcp/deploy/update.sh
@@ -1,7 +1,11 @@
 docker pull ghcr.io/stakwork/gevity-mcp:latest
+docker pull ghcr.io/stakwork/gevity-standalone:latest
 
 docker stop mcp.sphinx &> /dev/null
 docker rm mcp.sphinx &> /dev/null
+
+docker stop gevity.sphinx &> /dev/null
+docker rm gevity.sphinx &> /dev/null
 
 docker-compose up -d
 
